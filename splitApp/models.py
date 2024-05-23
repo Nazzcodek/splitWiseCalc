aa7365ee449f76ae82a373c5ee5b05c4a3982355
Nazzcodek <bellnas09@gmail.com>
2024-05-23 18:14:58 +0100

    modify the model and expense for the views

--- splitApp/models.py
+++ splitApp/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from uuid import uuid4
-from services import *
 
 
 class BaseModel(models.Model):
-    """This is the base model for fields associated with all models"""
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
 
 
 class User(BaseModel):
@@ -24,47 +25,24 @@ class Expense(BaseModel):
     title = models.CharField(max_length=255)
     description = models.TextField()
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    
+
     def __str__(self):
         return f"{self.title} - {self.amount}"
 
 
 class UserWallet(BaseModel):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    owes = models.JSONField(default=dict)
-    is_owed = models.JSONField(default=dict)
+    # removed the is_owed and owed field from the model and refractor to balances
+    balances = models.JSONField(default=dict)
+   
 
-    def add_owed(self, user_id, amount):
-        add_owed(self, user_id, amount)
-
-    def add_is_owed(self, user_id, amount):
-        add_is_owed(self, user_id, amount)
-
-    def check_balance(self):
-        return check_balance(self, self.__class__)
-
-    def __str__(self):
-        return self.check_balance()
-
-
-class ExpenseSharing(models.Model):
+class ExpenseSharing(BaseModel):
     expense = models.ForeignKey(Expense, on_delete=models.CASCADE)
-    method = models.CharField(max_length=10)
+    method = models.CharField(max_length=10, choices=[
+        ('EQUAL', 'Equal'),
+        ('EXACT', 'Exact'),
+        ('PERCENT', 'Percent')
+    ])
     split_with = models.ManyToManyField(User, blank=True)
-    values = models.JSONField(default=list)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def save(self, *args, **kwargs):
-        if not self.pk:
-            super().save(*args, **kwargs)
-            return
-
-        if calculate_expense_sharing_values(self):
-            self.apply_expense()
-
-        # Save again to update any changes
-        super().save(*args, **kwargs)
-
-    def apply_expense(self):
-        apply_expense(self)
+    # Added total shares field
+    total_shares = models.PositiveIntegerField()
--- splitApp/services.py
+++ splitApp/services.py
@@ -1,69 +1,64 @@
 """This is the service module for the splitWise app."""
 
+from .models import User, UserWallet, ExpenseSharing
+from django.db import transaction
+
 def add_owed(wallet, user_id, amount):
     user_id = str(user_id)
-    if user_id in wallet.owes:
-        wallet.owes[user_id] += amount
-        if wallet.owes[user_id] <= 0:
-            del wallet.owes[user_id]
-    else:
-        if user_id in wallet.is_owed:
-            wallet.is_owed[user_id] -= amount
-            if wallet.is_owed[user_id] <= 0:
-                del wallet.is_owed[user_id]
-        else:
-            wallet.owes[user_id] = amount
+    wallet.balances.setdefault(user_id, 0)
+    wallet.balances[user_id] += amount
+    if wallet.balances[user_id] == 0:
+        # Remove the user from the balances if the amount is 0
+        del wallet.balances[user_id]
     wallet.save()
+
 
 def add_is_owed(wallet, user_id, amount):
-    user_id = str(user_id)
-    if user_id in wallet.is_owed:
-        wallet.is_owed[user_id] += amount
-        if wallet.is_owed[user_id] <= 0:
-            del wallet.is_owed[user_id]
-        else:
-            if user_id in wallet.owes:
-                wallet.owes[user_id] -= amount
-                if wallet.owes[user_id] <= 0:
-                    del wallet.owes[user_id]
-            else:
-                wallet.is_owed[user_id] = amount
-    wallet.save()
+    add_owed(wallet, user_id, -amount)
 
-def check_balance(Cls, wallet):
-    if not wallet.is_owed:
+def check_balance(wallet):
+    if not wallet.balances:
         return "No balance"
     else:
         result = ""
-        for debtor_id, amount in wallet.is_owed.items():
-            debtor = Cls.objects.get(user_id=debtor_id)  # Fetch the User instance
-            result += f"{wallet.owner.username} owes {debtor.username}: ${amount}\n"
+        for user_id, amount in wallet.balances.items():
+            debtor = User.objects.get(user_id=user_id)
+            # Check if the user is owed to the wallet or owes to the wallet
+            # user is debitor
+            if amount > 0:
+                result += f"{wallet.owner.username} owes {debtor.username}: ${amount}\n"
+            # user is creditor
+            elif amount < 0:
+                result += f"{debtor.username} owes {wallet.owner.username}: ${-amount}\n"
         return result.strip()
 
 
 def calculate_expense_sharing_values(expense_sharing):
-    # Check if split_with field has users
-    if expense_sharing.split_with.exists():
-        if expense_sharing.method == "EQUAL":
-            expense_sharing.values = [round(expense_sharing.expense.amount / expense_sharing.split_with.count(), 2)] * expense_sharing.split_with.count()
-        elif expense_sharing.method == "EXACT":
-            if sum(expense_sharing.values) != expense_sharing.expense.amount:
-                raise ValueError("Total exact amounts must equal the total amount")
-        elif expense_sharing.method == "PERCENT":
-            total_percent = sum(expense_sharing.values)
-            if total_percent != 100:
-                raise ValueError("Total percentage must be 100")
-            expense_sharing.values = [round(amount / 100 * expense_sharing.expense.amount, 2) for amount in expense_sharing.values]
-        return True
-    else:
-        raise ValueError("No users to split the expense with")
+    amount = expense_sharing.expense.amount
+    num_shares = expense_sharing.total_shares
 
+    if expense_sharing.method == "EQUAL":
+        return [round(amount / num_shares, 2)] * num_shares
+    elif expense_sharing.method == "EXACT":
+        if sum(expense_sharing.values) != amount:
+            raise ValueError("Total exact amounts must equal the total amount")
+        return expense_sharing.values
+    elif expense_sharing.method == "PERCENT":
+        total_percent = sum(expense_sharing.values)
+        if total_percent != 100:
+            raise ValueError("Total percentage must be 100")
+        return [round(percent / 100 * amount, 2) for percent in expense_sharing.values]
+    else:
+        raise ValueError("Invalid splitting method")
+
+# I'm using transaction for data consistency so balances are updated atomically
+@transaction.atomic  
 def apply_expense(expense_sharing):
-    from models import UserWallet
+    shares = calculate_expense_sharing_values(expense_sharing)  
+
     for i, user in enumerate(expense_sharing.split_with.all()):
-        payer = expense_sharing.expense.paid_by
-        if user != payer:
+        if user != expense_sharing.expense.paid_by:
             user_wallet, _ = UserWallet.objects.get_or_create(owner=user)
-            payer_wallet, _ = UserWallet.objects.get_or_create(owner=payer)
-            user_wallet.add_owed(payer_wallet.owner_id, expense_sharing.values[i])
-            payer_wallet.add_is_owed(user_wallet.owner_id, expense_sharing.values[i])
+            payer_wallet, _ = UserWallet.objects.get_or_create(owner=expense_sharing.expense.paid_by)
+            user_wallet.update_balance(payer_wallet.owner_id, shares[i])
+            payer_wallet.update_balance(user_wallet.owner_id, -shares[i])
